7a2eeda520f31758c383d448bec4b447d42b0b54
Joe Hentges <joeypeterhentges@gmail.com>
2019-05-22 15:10:40 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Requirements
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('tiny'));
+
+// Static Files - Additionally grabbing Bootstrap and jQuery
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+
+// Set the HTML Template engine to EJS
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      title: 'hello'
+    }
+  );
+});
+
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
+});
